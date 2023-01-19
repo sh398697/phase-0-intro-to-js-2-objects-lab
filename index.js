254ce72e4e2b3797fdf67b64304a0f946fca9048
Scott <scotthenry1@gmail.com>
2023-01-19 15:00:56 -0500

    Completed lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Scott Henry",
+    streetAddress: "6 Highland Avenue",
+}
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+      };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = { ...obj };
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+    delete obj[key];
+    return obj;
+}
